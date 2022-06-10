b4304b92a0aa7cecd49f6f46556cfda10094f253
GolubDenis <99288665+GolubDenis@users.noreply.github.com>
2022-06-10 15:58:39 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,15 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
 #include "train.h"
-Train::Train() : countOp(0), first(nullptr) {
-}
 
-void Train::addCage(bool light) {
-    Cage* cage = new Cage;
-    cage->light = light;
-    if (first == nullptr) {
-        first = cage;
-        cage->next = cage;
-        cage->prev = cage;
-    } else {
-        cage->next = first;
-        cage->prev = first->prev;
-        first->prev = cage;
-        cage->prev->next = cage;
-    }
-}
+int main() {
+  Train train;
+  int count = 60; // кол-во вагонов
 
-int Train::getLength() {
-    first->light = true;
-    int len = 0;
-    while (first->light == true) {
-        Cage* item = first->next;
-        countOp += 2;
-        len = 1;
-        while (item->light == false) {
-            countOp += 2;
-            len++;
-            item = item->next;
-        }
-        item->light = false;
-    }
-    return len;
-}
+  while (count--)
+    train.addCage(false);
 
-int Train::getOpCount() {
-    return countOp;
+  std::cout << train.getLength() << std::endl;
+  std::cout << train.getOpCount() << std::endl;
+  return 0;
 }
